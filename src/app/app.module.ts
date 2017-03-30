e0c67f9c18a5845955250eec5842672824e361fd
snoctl <todd.lack@sas.com>
2017-03-30 16:15:39 -0400

    checkpoint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,22 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import {HeaderComponent} from "./header.component";
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    RecipesComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/header.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+/**
+ * Created by snoctl on 3/30/2017.
+ */
+
+@Component({
+  templateUrl:'./header.component.html',
+  selector : 'app-header'
+})
+export class HeaderComponent {
+
+}
